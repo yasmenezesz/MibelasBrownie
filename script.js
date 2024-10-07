cca385a2f0e858e688dd771de47a08b4295d8b9f
Yasmim Menezes <yasmimsilveira2009@gmail.com>
2024-10-07 18:30:29 -0300

    MibelasBrownie

--- script.js
+++ script.js
@@ -35,17 +35,5 @@ document.addEventListener('DOMContentLoaded', function() {
 			}
 		});
 	});
-
-	ScrollReveal().reveal('#cta', {
-		origin: 'left',
-		duration: 2000,
-		distance: '20%'
-	  });
-
-	  ScrollReveal().reveal('.feedback', {
-		origin: 'right',
-		duration: 1000,
-		distance: '20%'
-	  });
-	});
+});
 		
